b345dd76984dd39299eaf6d32c7f74e2c3d9b121
veryacademy <35196453+veryacademy@users.noreply.github.com>
2020-08-29 16:04:29 +0100

    nc

---
+++ FormView/books/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Books
+
+
+class AddForm(forms.ModelForm):
+
+    class Meta:
+        model = Books
+        fields = ('title', 'genre', 'author', 'isbn')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'genre': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'isbn': forms.TextInput(attrs={'class': 'form-control'}),
+        }
